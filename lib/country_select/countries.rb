6f5834ca79f1e0a9b4e5cc4405f59d73b5d384fa
Austin Rose <auzroz@gmail.com>
2013-10-27 18:46:52 -0700

    Changed Countries control structure to be more verbose.

--- lib/country_select/countries.rb
+++ lib/country_select/countries.rb
@@ -10,7 +10,11 @@ module CountrySelect
     @@use_iso_codes = use_iso_codes
   end
 
-  COUNTRIES =  ISO3166::Country.all.inject({}) {|r,s| r.merge!({s[1] => s[0]})} unless const_defined?("COUNTRIES")
+  unless const_defined?("COUNTRIES")
+    COUNTRIES =  ISO3166::Country.all.inject({}) do |r,s| 
+        r.merge!({s[1] => s[0]})
+    end
+  end
 
   ISO_COUNTRIES_FOR_SELECT = COUNTRIES.invert unless const_defined?("ISO_COUNTRIES_FOR_SELECT")
   COUNTRIES_FOR_SELECT = COUNTRIES.values unless const_defined?("COUNTRIES_FOR_SELECT")
