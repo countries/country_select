16beb190be7ba7b5d5d26807c9d21a37b896364e
Hsu Hlaing <hmhlaing@hotmail.com>
2014-09-17 19:53:40 +0100

    Should work without redefining add_options

--- lib/country_select/rails3/country_select_helper.rb
+++ lib/country_select/rails3/country_select_helper.rb
@@ -32,32 +32,8 @@ module ActionView
           html_options = @html_options.stringify_keys
           add_default_name_and_id(html_options)
           options[:include_blank] ||= true unless options[:prompt] || select_not_required?(html_options)
-          value = options.fetch(:selected) { value(object) }
-          content_tag(:select, add_options(country_option_tags, options, value), html_options)
+          content_tag(:select, add_options(country_option_tags, options, value(object)), html_options)
         end
-      end
-
-      def value(object)
-        object.send @method_name if object
-      end
-
-      def select_not_required?(html_options)
-        !html_options["required"] || html_options["multiple"] || html_options["size"].to_i > 1
-      end
-
-      def add_options(option_tags, options, value=nil)
-        if options[:include_blank]
-          option_tags = content_tag_string('option', options[:include_blank].kind_of?(String) ? options[:include_blank] : nil, :value => '') + "\n" + option_tags
-        end
-
-        if value.blank? && options[:prompt]
-          option_tags = content_tag_string('option', prompt_text(options[:prompt]), :value => '') + "\n" + option_tags
-        end
-        option_tags
-      end
-
-      def prompt_text(text)
-        text.kind_of?(String) ? text : I18n.translate('helpers.select.prompt', default: 'Please select')
       end
     end
   end
