1eeed7e4d54d9f5a8e0618b7ee9484d98df5100f
Mike Pluquin <mike.pluquin@immobilier.email>
2020-06-05 10:48:20 +0200

    Safely dig into country translations

--- lib/country_select/formats.rb
+++ lib/country_select/formats.rb
@@ -2,6 +2,6 @@ module CountrySelect
   FORMATS = {}
 
   FORMATS[:default] = lambda do |country|
-    country.translations[I18n.locale.to_s] || country.name
+    country.translations&.dig(I18n.locale.to_s) || country.name
   end
 end
